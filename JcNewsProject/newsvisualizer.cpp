e24c82b67d9eecc16e4f59ed8b531fbfce80cb8f
dev1d123 <dhuamanio@unsa.edu.pe>
2025-06-28 20:40:22 -0500

    add: load text

--- JcNewsProject/newsvisualizer.cpp
+++ JcNewsProject/newsvisualizer.cpp
@@ -1,7 +1,9 @@
 #include "newsvisualizer.h"
 #include "fakeresults.h"
 #include "ui_newsvisualizer.h"
-
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 NewsVisualizer::NewsVisualizer(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::NewsVisualizer)
@@ -9,9 +11,6 @@ NewsVisualizer::NewsVisualizer(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("Noticia");
-
-    // Mostrar resumen inicial
-    actualizarResumen();
 
 
     connect(ui->atrasButton, &QPushButton::clicked, this, &QDialog::reject);
@@ -31,14 +30,55 @@ NewsVisualizer::~NewsVisualizer()
 
 void NewsVisualizer::setIdUrl(const QString& id) {
     idUrl = id;
-    actualizarResumen();
 }
 
 void NewsVisualizer::setTextPath(const QString& path) {
     textPath = path;
-    actualizarResumen();
+
+    QFile file(textPath);
+    QString contenidoArchivo;
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&file);
+        contenidoArchivo = in.readAll();
+        file.close();
+    } else {
+        contenidoArchivo = "No se pudo abrir el archivo:\n" + textPath;
+    }
+
+    ui->contenidoText->setPlainText(contenidoArchivo);
 }
 
+
+void NewsVisualizer::setImgUrl(const QString& url) {
+    imgUrl = url;
+
+    // Descargar imagen desde internet
+    QUrl imageUrl(imgUrl);
+    if (imageUrl.isValid()) {
+        QNetworkRequest request(imageUrl);
+        networkManager->get(request);
+    } else {
+        ui->imagenLabel->setText("URL no válida");
+    }
+}
+
+void NewsVisualizer::imagenDescargada(QNetworkReply* reply)
+{
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray data = reply->readAll();
+        QPixmap imagen;
+        if (imagen.loadFromData(data)) {
+            QPixmap scaled = imagen.scaled(ui->imagenLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+            ui->imagenLabel->setPixmap(scaled);
+        } else {
+            ui->imagenLabel->setText("No se pudo cargar la imagen");
+        }
+    } else {
+        ui->imagenLabel->setText("Error al descargar imagen");
+    }
+
+    reply->deleteLater();
+}
 
 
 void NewsVisualizer::setFecha(const QDateTime& fecha)
@@ -60,47 +100,5 @@ void NewsVisualizer::setTitulo(const QString& titulo)
 {
     ui->tituloLine->setText(titulo);
 }
-void NewsVisualizer::setImgUrl(const QString& url) {
-    imgUrl = url;
-    actualizarResumen();
 
-    // Descargar imagen desde internet
-    QUrl imageUrl(imgUrl);
-    if (imageUrl.isValid()) {
-        QNetworkRequest request(imageUrl);
-        networkManager->get(request);
-    } else {
-        ui->imagenLabel->setText("URL no válida");
-    }
-}
-void NewsVisualizer::imagenDescargada(QNetworkReply* reply)
-{
-    if (reply->error() == QNetworkReply::NoError) {
-        QByteArray data = reply->readAll();
-        QPixmap imagen;
-        if (imagen.loadFromData(data)) {
-            QPixmap scaled = imagen.scaled(ui->imagenLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            ui->imagenLabel->setPixmap(scaled);
-        } else {
-            ui->imagenLabel->setText("No se pudo cargar la imagen");
-        }
-    } else {
-        ui->imagenLabel->setText("Error al descargar imagen");
-    }
-
-    reply->deleteLater();
-}
-
-
-void NewsVisualizer::actualizarResumen()
-{
-    if (!ui) return;
-
-    QString resumen;
-    resumen += "ID URL: " + idUrl + "\n";
-    resumen += "Ruta de texto: " + textPath + "\n";
-    resumen += "Enlace de imagen: " + imgUrl + "\n";
-
-    ui->contenidoText->setPlainText(resumen);
-}
 
